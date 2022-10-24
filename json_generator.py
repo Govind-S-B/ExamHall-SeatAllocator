19d234a63d2519709e53bed0e2bf8a852aab36c4
Aquila <reenacottage@gmail.com>
2022-10-24 20:57:38 +0530

    fixed bug

--- json_generator.py
+++ json_generator.py
@@ -1,43 +1,56 @@
 import json
 
-print("Input Mode: \nJSON File \nJSON text")
+list_to_generate = int(
+    input("Generate: \nHall List(1) \nSubject List(2)\n"))
 
-print("Generate: \n1. Hall List \n2. Subject List")
+if list_to_generate == 1:
+    halls = int(input("Enter number of halls: "))
 
-print("Enter number of halls")
+    Halls = {}
+    for i in range(halls):
+        hall_name = input(
+            "Enter Hall Name: ")
+        capacity = int(input("Enter the capacity: "))
+        columns = int(input("Enter the number of columns: "))
 
-print("Enter details in the format \nHall_Name,Capacity,Columns\n")
+        Halls[hall_name] = [capacity, columns]
 
-print("Enter input mode 1.Subject Roll List 2.Class Subject distribution")
+    with open('Halls.json', 'w') as fp:
+        json.dump(Halls, fp)
 
-# mode 1 :  // value mode ( most probably used by uni exam)
-#   subject name : mat102
-#   enter roll list :
-#       s2r1-32,s2r1-33,s2r1-34,s2r1-34
+elif list_to_generate == 2:
+    mode = int(input("Value mode(1) or Continuous Range mode(2): "))
+    Subjects = {}
 
-# mode 2 : // range mode
-#   enter number of classes : 75
-#   class name : s3r1
-#   enter number of subjects : 1
-#   enter subject : mat302
-#   enter roll number ranges : 1-63
+    if mode == 1:
+        subject_name = input("Enter the subject name: ")
+        subject_code = input("Enter the subject code: ")
+        Subjects[subject_name] = []
+        roll = input("Enter the roll number list: ")
+        roll_list = roll.split(',')
+        print(roll_list)
 
-# make input functions depending on the requirements
+        for roll_ in roll_list:
+            Subjects[subject_name].append(roll_)
 
+    elif mode == 2:
+        no_of_classes = int(input("Enter the number of classes: "))
+        for class_ in range(0, no_of_classes):
+            class_name = input("Enter class name")
+            no_of_subjects = int(input("Enter the number of subjects: "))
 
+            for subject_ in range(no_of_subjects):
+                subject = input("Enter Subject: ")
 
+                if subject in Subjects.keys():
+                    for roll_ in range(int(roll_no_range.split('-')[0]), int(roll_no_range.split('-')[1])):
+                        Subjects[subject].append(class_name + '-' + str(roll_))
+                else:
+                    Subjects[subject] = []
+                    roll_no_range = input("Enter roll number range: ")
 
-# Output requirements
-Halls = {"SJ310":[68,8],
-        "SJ308":[57,5]}
+                    for roll_ in range(int(roll_no_range.split('-')[0]), int(roll_no_range.split('-')[1])):
+                        Subjects[subject].append(class_name + '-' + str(roll_))
 
-Subjects = {"Discrete Mathematics":["s3r1-1","s3r1-2","s3r1-3","s3r1-4","s3r1-5","s3r1-6","s3r1-7","s3r1-8","s3r1-9"],
-            "Object Oriented Programming with Java":["s3r2-1","s3r2-2","s3r2-3","s3r2-4","s3r2-5","s3r2-6","s3r2-7","s3r2-8","s3r2-9"]}
-
-with open('Halls.json', 'w') as fp:
-    json.dump(Halls, fp)
-
-with open('Subjects.json', 'w') as fp:
-    json.dump(Subjects, fp, indent=4)
-
-# If teacher wants to fine control anything , or make verification , she can check the generated json file before running the report gen script
+    with open('Subjects.json', 'w') as fp:
+        json.dump(Subjects, fp, indent=4)
