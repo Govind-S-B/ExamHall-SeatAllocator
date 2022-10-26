e211e1b0fb1afd0bf61f1595c6d0d8621494d1b9
Aquila <reenacottage@gmail.com>
2022-10-27 00:18:14 +0530

    testing seatingPDF

--- Reportlab.py
+++ Reportlab.py
@@ -7,59 +7,57 @@ from reportlab.platypus import Table
 from reportlab.pdfgen import canvas
 import itertools
 
-#pinneyum pdf
+# pinneyum pdf
 
-def createpdf1(data, session, name): #session =list[hall name, session]
-    session1 = [[session,name]]
+
+def createpdf1(data, session, name):  # session =list[hall name, session]
+    session1 = [[session, name]]
     fileName = name+'.pdf'
     pdf = SimpleDocTemplate(
         fileName,
         pagesize=letter)
-    
 
     table = Table(data)
     table1 = Table(session1)
     # add style
 
-
     style = TableStyle([
-        ('BACKGROUND', (0,0), (3,0), colors.green),
-        ('TEXTCOLOR',(0,0),(-1,0),colors.whitesmoke),
+        ('BACKGROUND', (0, 0), (3, 0), colors.green),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
 
-        ('ALIGN',(0,0),(-1,-1),'CENTER'),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
 
-        ('FONTNAME', (0,0), (-1,0), 'Courier-Bold'),
-        ('FONTSIZE', (0,0), (-1,0), 14),
+        ('FONTNAME', (0, 0), (-1, 0), 'Courier-Bold'),
+        ('FONTSIZE', (0, 0), (-1, 0), 14),
 
-        ('BOTTOMPADDING', (0,0), (-1,0), 12),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
 
-        ('BACKGROUND',(0,1),(-1,-1),colors.beige),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
     ])
     table.setStyle(style)
 
-   
-    # 2) Alternate backgroud color. Make changes here if needed alt colours 
+    # 2) Alternate backgroud color. Make changes here if needed alt colours
     rowNumb = len(data)
     for i in range(1, rowNumb):
         if i % 2 == 0:
             bc = colors.white
         else:
             bc = colors.white
-        
+
         ts = TableStyle(
-            [('BACKGROUND', (0,i),(-1,i), bc)]
+            [('BACKGROUND', (0, i), (-1, i), bc)]
         )
         table.setStyle(ts)
 
     # 3) Add borders
     ts = TableStyle(
         [
-        ('BOX',(0,0),(-1,-1),2,colors.black),
+            ('BOX', (0, 0), (-1, -1), 2, colors.black),
 
-        ('LINEBEFORE',(2,1),(2,-1),2,colors.red),
-        ('LINEABOVE',(0,2),(-1,2),2,colors.green),
+            ('LINEBEFORE', (2, 1), (2, -1), 2, colors.red),
+            ('LINEABOVE', (0, 2), (-1, 2), 2, colors.green),
 
-        ('GRID',(0,1),(-1,-1),2,colors.black),
+            ('GRID', (0, 1), (-1, -1), 2, colors.black),
         ]
     )
     table.setStyle(ts)
@@ -70,74 +68,73 @@ def createpdf1(data, session, name): #session =list[hall name, session]
     elems.append(table)
     pdf.build(elems)
 
+
 def createpdf(data, name):
 
     fileName = name+'.pdf'
     pdf = SimpleDocTemplate(
         fileName,
         pagesize=letter)
-    
 
     table = Table(data)
 
     # add style
 
-
     style = TableStyle([
-        ('BACKGROUND', (0,0), (3,0), colors.green),
-        ('TEXTCOLOR',(0,0),(-1,0),colors.whitesmoke),
+        ('BACKGROUND', (0, 0), (3, 0), colors.green),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
 
-        ('ALIGN',(0,0),(-1,-1),'CENTER'),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
 
-        ('FONTNAME', (0,0), (-1,0), 'Courier-Bold'),
-        ('FONTSIZE', (0,0), (-1,0), 14),
+        ('FONTNAME', (0, 0), (-1, 0), 'Courier-Bold'),
+        ('FONTSIZE', (0, 0), (-1, 0), 14),
 
-        ('BOTTOMPADDING', (0,0), (-1,0), 12),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
 
-        ('BACKGROUND',(0,1),(-1,-1),colors.beige),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
     ])
     table.setStyle(style)
     style1 = TableStyle([
-        ('TEXTCOLOR',(0,0),(-1,0),colors.black),
-        ('ALIGN',(0,0),(-1,-1),'CENTER'),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.black),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
     ])
 
-   
-    # 2) Alternate backgroud color. Make changes here if needed alt colours 
+    # 2) Alternate backgroud color. Make changes here if needed alt colours
     rowNumb = len(data)
     for i in range(1, rowNumb):
         if i % 2 == 0:
             bc = colors.white
         else:
             bc = colors.white
-        
+
         ts = TableStyle(
-            [('BACKGROUND', (0,i),(-1,i), bc)]
+            [('BACKGROUND', (0, i), (-1, i), bc)]
         )
         table.setStyle(ts)
 
     # 3) Add borders
     ts = TableStyle(
         [
-        ('BOX',(0,0),(-1,-1),2,colors.black),
+            ('BOX', (0, 0), (-1, -1), 2, colors.black),
 
-        ('LINEBEFORE',(2,1),(2,-1),2,colors.red),
-        ('LINEABOVE',(0,2),(-1,2),2,colors.green),
+            ('LINEBEFORE', (2, 1), (2, -1), 2, colors.red),
+            ('LINEABOVE', (0, 2), (-1, 2), 2, colors.green),
 
-        ('GRID',(0,1),(-1,-1),2,colors.black),
+            ('GRID', (0, 1), (-1, -1), 2, colors.black),
         ]
     )
     table.setStyle(ts)
 
     elems = []
-    
+
     elems.append(table)
     pdf.build(elems)
+
 
 conn = sq.connect("report.db")
 
 
-# Notice board 
+# Notice board
 
 cmd = """SELECT CLASS,HALL,ROLL
          FROM REPORT
@@ -151,35 +148,37 @@ hall_name = Q_list[0][1]
 
 for i in Q_list:
     if class_name == i[0]:
-        
+
         if hall_name == i[1]:
             roll_list.append(i[2])
         else:
-            PDF_list.append([class_name,hall_name,str(roll_list)[1:-1]])
+            PDF_list.append([class_name, hall_name, str(roll_list)[1:-1]])
             hall_name = i[1]
             roll_list = []
             roll_list.append(i[2])
 
     else:
-        PDF_list.append([class_name,hall_name,str(roll_list)[1:-1]])
+        PDF_list.append([class_name, hall_name, str(roll_list)[1:-1]])
         class_name = i[0]
         hall_name = i[1]
         roll_list = []
         roll_list.append(i[2])
 
     if Q_list[-1] == i:
-        PDF_list.append([class_name,hall_name,str(roll_list)[1:-1]])
+        PDF_list.append([class_name, hall_name, str(roll_list)[1:-1]])
 
 #createpdf(PDF_list, "PDF1")
 
 
-
 sessioninfo = "12-04-2023 FN"
 # For Each Hall Packaging
+
+
 def ranges(i):
     for a, b in itertools.groupby(enumerate(i), lambda pair: pair[1] - pair[0]):
         b = list(b)
         yield b[0][1], b[-1][1]
+
 
 conn = sq.connect("report.db")
 cmd = """SELECT HALL,CLASS,SUBJECT,ROLL
@@ -204,7 +203,8 @@ for i in Q_list:
 
             else:
                 roll_ = ranges(roll_list)
-                PDF_list.append([class_name, subject_name, str(list(roll_))[1:-1]])
+                PDF_list.append(
+                    [class_name, subject_name, str(list(roll_))[1:-1]])
                 subject_name = i[2]
                 roll_list = []
                 roll_list.append(i[3])
@@ -221,8 +221,8 @@ for i in Q_list:
     else:
         # append , PDF Generate and empty pdf list
         roll_ = ranges(roll_list)
-        PDF_list.append([class_name, subject_name,str(list(roll_))[1:-1]])
-        createpdf1(PDF_list,sessioninfo,  hall_name)
+        PDF_list.append([class_name, subject_name, str(list(roll_))[1:-1]])
+        createpdf1(PDF_list, sessioninfo,  hall_name)
         PDF_list = []
         hall_name = i[0]
         class_name = i[1]
@@ -230,14 +230,42 @@ for i in Q_list:
         roll_list.append(i[3])
 
     if Q_list[-1] == i:
-        #PDF Generate
+        # PDF Generate
 
         roll_ = ranges(roll_list)
-        PDF_list.append([class_name, subject_name,str(list(roll_))[1:-1]])
-        createpdf1(PDF_list,sessioninfo,  hall_name)
+        PDF_list.append([class_name, subject_name, str(list(roll_))[1:-1]])
+        createpdf1(PDF_list, sessioninfo,  hall_name)
         PDF_list = []
 
 
+# seating list
+
+cmd = """SELECT DISTINCT HALL , CLASS
+         FROM REPORT
+         ORDER BY HALL"""
+cursor = conn.execute(cmd)
+x = cursor.fetchall()
+distinct_class = []
+for i in x:
+    distinct_class.append(list(i))
+
+cmd = """SELECT HALL,SEAT_NO,ID
+         FROM REPORT
+         ORDER BY HALL,SEAT_NO"""
+cursor = conn.execute(cmd)
+x = cursor.fetchall()
+seat_list = []
+for i in x:
+    seat_list.append(list(i))
+
+hall = seat_list[0][0]
+seat_List = []
+for i in seat_list:
+    if hall == i[0]:
+        seat_List.append([i[1], i[2]])
+    else:
+        createpdf1(seat_List, sessioninfo, distinct_class)
+        seat_List = []
 
 
-
+# distinct class
