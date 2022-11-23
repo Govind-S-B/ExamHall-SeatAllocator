c0674a057dfcca5b9ae33b3d094d667087501b79
Govind-S-B <b.s.dnivog@gmail.com>
2022-11-24 04:11:01 +0530

    experimental features

---
+++ v2_experiment/json_generator.py
@@ -0,0 +1,69 @@
+import json
+
+output_mode = int(input("Display Mode: (1)Text (2)JSON: "))
+
+list_to_generate = int(
+    input("Generate: Hall List(1) Subject List(2): "))
+
+if list_to_generate == 1:
+    halls = int(input("Enter number of halls: "))
+
+    Halls = {}
+    for i in range(halls):
+        hall_name = input("Hall Name: ")
+        capacity = int(input("Table Count : "))
+
+        Halls[hall_name] = [capacity]
+
+    if output_mode == 2:
+        with open('Halls.json', 'w') as fp:
+            json.dump(Halls, fp)
+    elif output_mode == 1:
+        print(Halls)
+
+elif list_to_generate == 2:
+
+    session_name = input("Enter Session name: ")  # 12-04-2022 FN
+    MetaInfo = {"Session_Name": session_name}
+    Subjects = {}
+    Subjects["meta"] = MetaInfo
+
+    no_of_classes = int(input("Enter number of classes: "))
+    for class_ in range(0, no_of_classes):
+        class_name = input("Enter class name: ")
+        no_of_subjects = int(input("Enter number of subjects: "))
+
+        for subject_ in range(no_of_subjects):
+            subject = input("Enter Subject: ")
+
+            if subject in Subjects:
+                roll = input("Enter roll number list: ")
+                roll_list = roll.split(',')
+                for item in roll_list:
+                    if "-" in item:
+                        roll_no_range = item
+                        for roll_ in range(int(roll_no_range.split('-')[0]), int(roll_no_range.split('-')[1])+1):
+                            Subjects[subject].append(class_name + '-' + str(roll_))
+                    else:
+                        Subjects[subject].append(class_name + '-' + item)
+            else:
+                Subjects[subject] = []
+
+                roll = input("Enter roll number list: ")
+                roll_list = roll.split(',')
+                for item in roll_list:
+                    if "-" in item:
+                        roll_no_range = item
+                        for roll_ in range(int(roll_no_range.split('-')[0]), int(roll_no_range.split('-')[1])+1):
+                            Subjects[subject].append(class_name + '-' + str(roll_))
+                    else:
+                        Subjects[subject].append(class_name + '-' + item)
+
+    if output_mode == 1:
+        print(json.dumps(Subjects, indent=4, sort_keys=True))
+
+    elif output_mode == 2:
+        with open('Subjects.json', 'w') as fp:
+            json.dump(Subjects, fp, indent=4)
+
+input("\n Enter any key to exit ")  # dummy input function to wait for user input to exit script
---
+++ v2_experiment/report_genv2.py
@@ -0,0 +1,127 @@
+import json
+import sqlite3 as sq
+
+
+# importing json data files (inputs)
+with open('Halls.json', 'r') as JSON:
+    Halls = json.load(JSON)
+
+with open('Subjects.json', 'r') as JSON:
+    Subjects = json.load(JSON)
+
+MetaInfo = Subjects.pop("meta") # Meta info global for each generation
+print(MetaInfo["Session_Name"])
+
+# setting up sqlite DB for processed or sorted data storage ( allocated seats )
+conn = sq.connect("report.db")
+conn.execute("DROP TABLE IF EXISTS REPORT;")
+conn.execute('''CREATE TABLE REPORT
+         (ID         CHAR(15)         PRIMARY KEY     NOT NULL,
+         CLASS       CHAR(10)                         NOT NULL,
+         ROLL        INT                              NOT NULL,
+         HALL        TEXT                             NOT NULL,
+         SEAT_NO     INT                              NOT NULL,
+         SUBJECT     CHAR(50)                         NOT NULL);''')
+
+
+Halls_list = [] # List of halls
+
+for i in Halls:
+    Halls_list.append([Halls[i][0],i]) # Hall Capacity , Hall name
+
+Halls_list = sorted(Halls_list, key = lambda x: x[0],reverse=True) # Sorting by capacity 
+
+Subjects_list = [] # List of subjects
+Students_total=0
+
+for i in Subjects:
+    Subjects_list.append([len(Subjects[i]),i] + Subjects[i]) # No of students , Sub Name , roll nos ...
+    Students_total+=len(Subjects[i])
+print("Total students: ",Students_total)
+
+Subjects_list = sorted(Subjects_list, key = lambda x: x[0],reverse=True) # Sorting by number of students
+
+even_row_subject_list = []
+odd_row_subject_list = []
+
+for i in range(len(Subjects_list)):
+    if i%2==0: #even
+        even_row_subject_list.append(Subjects_list[i])
+    else: #odd
+        odd_row_subject_list.append(Subjects_list[i])
+
+allocation_done = False
+Halls_sorted_list = []
+Student_allocated_count=0
+
+for i in Halls_list:
+    if allocation_done == True:
+            break
+    else:
+        Hall_name = i[1]
+        Hall_capacity = i[0]*2
+
+        for seat in range(1,Hall_capacity+1):
+            if allocation_done == True:
+                break
+            else:
+                if seat%2==0: #even
+                    if len(even_row_subject_list) == 0:
+                        pass
+                    else:
+                        x = even_row_subject_list[0].pop(2) #roll
+                        temp_expression = x.split("-")
+                        conn.execute(f"INSERT INTO REPORT VALUES('{x}','{temp_expression[0]}','{temp_expression[1]}','{Hall_name}','{seat}','{even_row_subject_list[0][1]}')")
+                        conn.commit()
+                        Student_allocated_count+=1
+                        even_row_subject_list[0][0]-=1
+                        if even_row_subject_list[0][0]==0:
+                             even_row_subject_list.pop(0)
+                else: #odd
+                    if len(odd_row_subject_list) == 0:
+                        pass
+                    else:
+                        x = odd_row_subject_list[0].pop(2) #roll
+                        temp_expression = x.split("-")
+                        conn.execute(f"INSERT INTO REPORT VALUES('{x}','{temp_expression[0]}','{temp_expression[1]}','{Hall_name}','{seat}','{odd_row_subject_list[0][1]}')")
+                        conn.commit()
+                        Student_allocated_count+=1
+                        odd_row_subject_list[0][0]-=1
+                        if odd_row_subject_list[0][0]==0:
+                             odd_row_subject_list.pop(0)
+
+                if (len(even_row_subject_list)==0) and (len(odd_row_subject_list)==0):
+                    allocation_done = True
+
+if allocation_done == False:
+    print()
+    print("Hall capacity insufficient.")
+    print("Number of students allocated: ",Student_allocated_count)
+    print("Number of students left to allocate: ",Students_total-Student_allocated_count)
+    input("\n Enter any key to exit ")
+
+
+
+# print(Halls_sorted_list)
+#print("Students allocated: ",Student_allocated_count)
+
+""" DEBUG
+for i in range(len(Halls_sorted_list)):
+    for j in range(len(Halls_sorted_list[i])): # hall
+        print(Halls_sorted_list[i][j])
+    print()
+"""
+
+# Report Generation
+
+# use meta data
+# use query
+# print report 1
+
+# use meta data
+# use query
+# print report 2
+
+# use meta data
+# use query
+# print report 3
