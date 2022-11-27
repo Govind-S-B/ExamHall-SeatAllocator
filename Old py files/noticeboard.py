76c292a47f529f12803ed277298893640798bb81
Siby C R <sibycr18@gmail.com>
2022-11-27 20:40:18 +0530

    Integrated pdf generating files to single py file

--- Old py files/noticeboard.py
+++ Old py files/noticeboard.py
@@ -65,87 +65,68 @@ for i in Q_list:
     if Q_list[-1] == i:
         temp = "("+str(len(roll_list))+")"
         roll_list.append(temp)
-        # print(roll_list)
         PDF_list.append([class_name, hall_name, roll_list])
-
-# # print Notice Board PDF on terminal----
-# print()
-# print()
-# print("Marian Engineering College")
-# print()
-# print("Halls for Internal Examination")
-# print("Date: ",Date,"   Session: ",Session)
-# print()
-# for i in PDF_list:
-#     print(i)
-# print()
-# print()
-# # -----------------------------------
-
 
 # fpdf Class and Object Creation
 class PDF(FPDF):
     # Page footer
     def footer(self):
         self.set_y(-15)
-        text_w=pdf.get_string_width("Created by ProtoRes")+6
-        self.set_x(((doc_w - text_w) / 2)+8)
+        text_w=pdf1.get_string_width("Created by ProtoRes")+6
+        self.set_x(((self.w - text_w) / 2)+7)
         self.set_font(font, '', 8)
-        self.cell(pdf.get_string_width("Created by "), 10, "Created by ")
+        self.cell(pdf1.get_string_width("Created by "), 10, "Created by ")
         self.set_font(font, 'B', 8)
-        self.cell(pdf.get_string_width("ProtoRes"), 10, "ProtoRes")
+        self.cell(pdf1.get_string_width("ProtoRes"), 10, "ProtoRes")
         self.set_font('helvetica', '', 8)
         # Page number
         self.cell(0, 10, f'{self.page_no()}/{{nb}}', align='R')
 
 
-pdf = PDF('P', 'mm', 'A4')
-
-pdf.set_auto_page_break(auto = True, margin = 15) # Set auto page break
-pdf.add_page()
+pdf1 = PDF('P', 'mm', 'A4')
 
 # adding fonts
 try:
-    pdf.add_font('Poppins', '', 'Fonts/Poppins-Regular.ttf')
-    pdf.add_font('Poppins', 'B', 'Fonts/Poppins-Bold.ttf')
+    pdf1.add_font('Poppins', '', 'Fonts/Poppins-Regular.ttf')
+    pdf1.add_font('Poppins', 'B', 'Fonts/Poppins-Bold.ttf')
     font="Poppins"
 except:
     print("Poppins font not found. Using Times now.")
     font="Times"
 
+pdf1.set_auto_page_break(auto = True, margin = 15) # Auto page break
+pdf1.add_page()
 
-pdf.set_font(font, '', 27)
+pdf1.set_font(font, '', 27)
 text="Marian Engineering College"
-text_w=pdf.get_string_width(text)+6
-doc_w=pdf.w
-pdf.set_x((doc_w - text_w) / 2)
-pdf.cell(text_w, 23, text,  new_x="LMARGIN", new_y="NEXT", align='C')
+text_w=pdf1.get_string_width(text)+6
+doc_w=pdf1.w
+pdf1.set_x((doc_w - text_w) / 2)
+pdf1.cell(text_w, 23, text,  new_x="LMARGIN", new_y="NEXT", align='C')
 
-pdf.set_font(font, '', 21)
+pdf1.set_font(font, '', 21)
 text="Halls for Internal Examination"
-text_w=pdf.get_string_width(text)+6
-pdf.set_x((doc_w - text_w) / 2)
-pdf.cell(text_w, 7, text,  new_x="LMARGIN", new_y="NEXT", align='C')
+text_w=pdf1.get_string_width(text)+6
+pdf1.set_x((doc_w - text_w) / 2)
+pdf1.cell(text_w, 7, text,  new_x="LMARGIN", new_y="NEXT", align='C')
 
-text_w=pdf.get_string_width("Date: 12-04-2022         Session: FN")
-pdf.set_x(((doc_w - text_w) / 2)+10.5)
+text_w=pdf1.get_string_width("Date: 12-04-2022         Session: FN")
+pdf1.set_x(((doc_w - text_w) / 2)+10.5)
 
-pdf.set_font(font, '', 17)
-pdf.cell(pdf.get_string_width("Date:"), 14, 'Date:', align='C')
-pdf.set_font(font, 'B', 17)
-pdf.cell(pdf.get_string_width(Date), 14, Date)
-pdf.set_font(font, '', 17)
-pdf.cell(pdf.get_string_width("         Session: "), 14, '         Session: ')
-pdf.set_font(font, 'B', 17)
-pdf.cell(pdf.get_string_width(Session), 14, Session,  new_x="LMARGIN", new_y="NEXT")
-
+pdf1.set_font(font, '', 17)
+pdf1.cell(pdf1.get_string_width("Date:"), 14, 'Date:', align='C')
+pdf1.set_font(font, 'B', 17)
+pdf1.cell(pdf1.get_string_width(Date), 14, Date)
+pdf1.set_font(font, '', 17)
+pdf1.cell(pdf1.get_string_width("         Session: "), 14, '         Session: ')
+pdf1.set_font(font, 'B', 17)
+pdf1.cell(pdf1.get_string_width(Session), 14, Session,  new_x="LMARGIN", new_y="NEXT")
 
 #Create Table Header
-pdf.set_font(font, 'B', 12)
-pdf.cell(22.1, 10, "Class", align='C', border=True)
-pdf.cell(18.5, 10, "Hall", align='C', border=True)
-pdf.cell(0, 10, "Roll No.s", align='C', border=True, new_x="LMARGIN", new_y="NEXT")
-
+pdf1.set_font(font, 'B', 12)
+pdf1.cell(22.1, 10, "Class", align='C', border=True)
+pdf1.cell(18.5, 10, "Hall", align='C', border=True)
+pdf1.cell(0, 10, "Roll No.s", align='C', border=True, new_x="LMARGIN", new_y="NEXT")
 
 # Create Table Body
 prev_class=""
@@ -170,21 +151,20 @@ for i in PDF_list:
             temp+=(str(j)+", ")
         temp_count+=1
 
-
     curr_class=i[0]
     height=rows*12
 
-    pdf.set_font(font, 'B', 11)
+    pdf1.set_font(font, 'B', 11)
     if prev_class==curr_class:
-        pdf.cell(22.1, height, '"', align='C', border=True)
+        pdf1.cell(22.1, height, '"', align='C', border=True)
     else:
-        pdf.cell(22.1, height, curr_class, align='C', border=True)
+        pdf1.cell(22.1, height, curr_class, align='C', border=True)
     prev_class=curr_class
-    pdf.set_font(font, '', 11)
-    pdf.cell(18.5, height, i[1], align='C', border=True, new_x="RIGHT")
+    pdf1.set_font(font, '', 11)
+    pdf1.cell(18.5, height, i[1], align='C', border=True, new_x="RIGHT")
 
-    pdf.multi_cell(0, height/rows, temp, new_x="LMARGIN", new_y="NEXT", border=True, align="L")
+    pdf1.multi_cell(0, height/rows, temp, new_x="LMARGIN", new_y="NEXT", border=True, align="L")
 
 file_name="Notice Board "+Date+" "+Session+".pdf"
-pdf.output(file_name)
+pdf1.output(file_name)
 #-----------------------------------------------------------------------------------------------------
