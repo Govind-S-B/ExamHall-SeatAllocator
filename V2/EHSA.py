f5a566178ec6744b07e124b98e836675ed83db40
officiallyaninja <officiallyaninja@gmail.com>
2022-12-25 15:09:05 +0530

    AHHHHHHH

--- V2/EHSA.py
+++ V2/EHSA.py
@@ -69,13 +69,15 @@ def generate_JSON():
         if output_mode not in [1, 2]:
             print("please type 1 or 2")
             continue
+        break
 
     while True:
         list_to_generate = int(input("Generate: (1)Hall List, (2)Subject List: "))
 
-        if output_mode not in [1, 2]:
+        if list_to_generate not in [1, 2]:
             print("please type 1 or 2")
             continue
+        break
 
     if list_to_generate == 1: # Hall List
 
