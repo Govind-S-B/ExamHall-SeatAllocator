3c9034c25c06494a36f733469c6cc3a9b3d2171d
officiallyaninja <officiallyaninja@gmail.com>
2023-05-23 18:05:59 +0530

    fixed git ignore

--- allocator/target/debug/deps/allocator-3feaa0f20a2aed9e.d
+++
@@ -1,5 +0,0 @@
-D:\Programming Projects\Multi language projects\ExamHall-SeatAllocator\allocator\target\debug\deps\allocator-3feaa0f20a2aed9e.rmeta: src\main.rs
-
-D:\Programming Projects\Multi language projects\ExamHall-SeatAllocator\allocator\target\debug\deps\allocator-3feaa0f20a2aed9e.d: src\main.rs
-
-src\main.rs:
