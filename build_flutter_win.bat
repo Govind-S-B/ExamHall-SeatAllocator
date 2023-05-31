aec7fba195c2beabf6f6bbdc09da65733282994b
Govind-S-B <b.s.dnivog@gmail.com>
2023-05-31 18:50:44 +0530

    updated bat file to include flutter_windows.dll

--- build_flutter_win.bat
+++ build_flutter_win.bat
@@ -1,2 +1,2 @@
-rmdir /s /q EHSA_V3\data && del /q EHSA_V3\ehsa_frontend.exe
-cd ehsa_frontend && flutter build windows && cd ../ && move ehsa_frontend\build\windows\runner\Release\data EHSA_V3 && move ehsa_frontend\build\windows\runner\Release\ehsa_frontend.exe EHSA_V3
+rmdir /s /q EHSA_V3\data && del /q EHSA_V3\ehsa_frontend.exe del /q EHSA_V3\flutter_windows.dll
+cd ehsa_frontend && flutter build windows && cd ../ && move ehsa_frontend\build\windows\runner\Release\data EHSA_V3 && move ehsa_frontend\build\windows\runner\Release\ehsa_frontend.exe EHSA_V3 && move ehsa_frontend\build\windows\runner\Release\flutter_windows.exe EHSA_V3
