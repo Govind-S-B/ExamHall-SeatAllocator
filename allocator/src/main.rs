8419394bf001c4a643c4f7fe07e4a252e3f2db1c
officiallyaninja <officiallyaninja@gmail.com>
2023-06-26 00:32:27 +0530

    moved complex logic out of get next student into main

--- allocator/src/main.rs
+++ allocator/src/main.rs
@@ -2,11 +2,10 @@ mod args;
 mod db_manager;
 mod hall;
 mod student;
-use hall::Hall;
-use student::Student;
 
 use rand::seq::SliceRandom;
 use std::collections::{HashMap, HashSet};
+use student::Student;
 
 enum AllocationMode {
     SeperateSubject,
@@ -24,6 +23,7 @@ fn main() {
 
     let mut allocation_mode = AllocationMode::SeperateSubject;
     let mut placed_keys = HashSet::new();
+    let mut previously_placed_key: Option<String> = None;
 
     let mut extra_seats = {
         let total_seats: usize = halls.iter().map(|h| h.seats_left()).sum();
@@ -42,9 +42,15 @@ fn main() {
 
         while !hall.is_full() && !students.is_empty() {
             // happy path, student is added to hall
-            if let Some(next_student) = get_next_student(&mut students, hall, &mut placed_keys) {
+            if let Some(next_key) =
+                get_next_key(&students, &placed_keys, previously_placed_key.as_ref())
+            {
+                let next_student = get_next_student(&mut students, &next_key);
+
                 hall.push(next_student)
                     .expect("tried to push student into full hall");
+                placed_keys.insert(next_key.clone());
+                previously_placed_key = Some(next_key);
                 continue;
             }
 
@@ -52,6 +58,7 @@ fn main() {
             if extra_seats > 0 {
                 hall.push_empty()
                     .expect("tried to push empty on full hall (error should never happer)");
+                previously_placed_key = None;
                 extra_seats -= 1;
                 continue;
             }
@@ -67,7 +74,7 @@ fn main() {
             // placed keys and previously placed key to reglect this
             allocation_mode = AllocationMode::SeperateClass;
             placed_keys.clear();
-            hall.previously_placed_key = hall
+            previously_placed_key = hall
                 .students()
                 .last()
                 .unwrap_or(&None)
@@ -98,16 +105,10 @@ fn main() {
     let conn = sqlite::open(args.output_db_path).expect("Error connecting to report.db");
     db_manager::write_report_table(&conn, &halls);
 }
-
-fn get_next_student(
-    students: &mut HashMap<String, Vec<Student>>,
-    hall: &mut Hall,
-    placed_keys: &mut HashSet<String>,
-) -> Option<Student> {
-    let next_key = get_next_key(students, hall, placed_keys)?;
-
+/// also clears entries as they get empty
+fn get_next_student(students: &mut HashMap<String, Vec<Student>>, key: &str) -> Student {
     let students_in_key = students
-        .get_mut(&next_key)
+        .get_mut(key)
         .expect("trying to take a student from subject that doesn't exist");
 
     let next_student = students_in_key
@@ -115,23 +116,21 @@ fn get_next_student(
         .expect("trying to take student from empty subject list");
 
     if students_in_key.is_empty() {
-        students.remove(&next_key);
+        students.remove(key);
     }
-    hall.previously_placed_key = Some(next_key.clone());
-    placed_keys.insert(next_key);
 
-    Some(next_student)
+    next_student
 }
 
 fn get_next_key(
     students: &HashMap<String, Vec<Student>>,
-    hall: &Hall,
     placed_keys: &HashSet<String>,
+    previously_placed_key: Option<&String>,
 ) -> Option<String> {
     let filtered = students
         .iter()
         .map(|(key, vec)| (key, vec.len()))
-        .filter(|(key, size)| Some(*key) != hall.previously_placed_key.as_ref() && *size > 0);
+        .filter(|(key, size)| Some(*key) != previously_placed_key && *size > 0);
 
     let further_filtered: Vec<(&String, usize)> = filtered
         .clone()
