e6c7b1e761377a446921a1d407d9f47d965b91f9
officiallyaninja <officiallyaninja@gmail.com>
2023-06-25 18:34:17 +0530

    switched order of error printing

--- allocator/src/args.rs
+++ allocator/src/args.rs
@@ -15,11 +15,10 @@ pub fn get_args() -> Arguments {
     }
     // first argument is the path of the program itself
     let [_, input_db_path, output_db_path] = args.as_slice()
-        else {
-            eprintln!("INVALID ARGUMENTS: arguments should be 
-             \" ./allocator <input_db_path> <output_db_path> [-r | --randomize]");
-            panic!("args given are {:?}", args);
-        };
+    else {
+        eprintln!("args given are {:?}", args);
+        panic!("INVALID ARGUMENTS: arguments should be \" ./allocator <input_db_path> <output_db_path> [-r | --randomize]");
+    };
     Arguments {
         randomize,
         input_db_path: input_db_path.to_owned(),
