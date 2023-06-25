959c84c37905ab96f9a7facb433729b42869c486
officiallyaninja <officiallyaninja@gmail.com>
2023-06-25 18:29:02 +0530

    Update args.rs

--- allocator/src/args.rs
+++ allocator/src/args.rs
@@ -6,19 +6,19 @@ pub struct Arguments {
 
 pub fn get_args() -> Arguments {
     let mut randomize = false;
-    let mut paths = Vec::with_capacity(3);
+    let mut args = Vec::with_capacity(3);
     for arg in std::env::args() {
         match arg.as_str() {
             "-r" | "--randomize" => randomize = true,
-            _ => paths.push(arg),
+            _ => args.push(arg),
         }
     }
     // first argument is the path of the program itself
-    let [_, input_db_path, output_db_path] = paths.as_slice()
+    let [_, input_db_path, output_db_path] = args.as_slice()
         else {
             eprintln!("INVALID ARGUMENTS: arguments should be 
              \" ./allocator <input_db_path> <output_db_path> [-r | --randomize]");
-            panic!("args given are {:?}", paths);
+            panic!("args given are {:?}", args);
         };
     Arguments {
         randomize,
