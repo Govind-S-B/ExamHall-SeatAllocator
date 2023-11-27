9cabd4450e149b092fedadaf0b3e5770f86bc1bc
Arjun Pratap <officiallyaninja@gmail.com>
2023-11-27 21:57:15 +0530

    init

--- allocator/test/build.bat
+++ allocator/test/build.bat
@@ -2,3 +2,4 @@ cd ..
 cargo build
 move target\debug\allocator.exe test\bin
 del test\logs\logs.txt
+cd test
--- allocator/test/test.bat
+++ allocator/test/test.bat
@@ -1,1 +1,6 @@
-.\bin\allocator.exe bin/input.db bin/report.db %1 %2
+set ORIGINAL_DIR=%CD%
+cd output/reports
+py ../../bin/run_tests.py
+cd ../pdfs
+chdir /d %ORIGINAL_DIR%
+
