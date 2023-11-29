202d812b43ab4285cabb74cf9bd0afebafb9bfd2
Arjun Pratap <officiallyaninja@gmail.com>
2023-11-29 23:13:45 +0530

    improved testing done

--- allocator/test/bin/debug_halls.py
+++ allocator/test/bin/debug_halls.py
@@ -10,7 +10,7 @@ LETTERS = [
     "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 ]
 
-COLORS = [
+CLASS_COLORS = [
     "black", "red", "green", "yellow", "blue", "magenta", "cyan", "white",
     "light_grey", "dark_grey", "light_red", "light_green", "light_yellow",
     "light_blue", "light_magenta", "light_cyan"
@@ -36,12 +36,10 @@ BACKGROUNDS = [
 ]
 
 
-# Replace <directory_path> with the actual directory path
 directory_path = r"..\output"
 sample_data = os.listdir(directory_path)
 
 for file_name in sample_data:
-    # Replace <directory_path> with the actual directory path
     file_path = os.path.join(directory_path, file_name)
     # print(f"{file_path=}, {file_name=}")
     # print(file_path, file_name, sep=';')
@@ -52,16 +50,30 @@ for file_name in sample_data:
     cmd = "SELECT DISTINCT subject FROM report ;"
     subjects = [row[0] for row in conn.execute(cmd)]
     letter = {sub: letter for (sub, letter) in zip(subjects, LETTERS)}
-    print(letter)
+    # print(letter)
 
     cmd = "SELECT DISTINCT class FROM report ;"
     classes = [row[0] for row in conn.execute(cmd)]
-    color = {class_name: color for (
-        class_name, color) in zip(classes, product(BACKGROUNDS, COLORS))}
-    print(color)
+    color_map = {class_name: color for (
+        class_name, color) in zip(classes, product(BACKGROUNDS, CLASS_COLORS))}
+    # print(color)
 
     # if len(subjects) > len(LETTERS) or len(classes) > len(COLORS):
     #     print(f"{len(subjects)=}, {len(LETTERS)=}, {len(classes)=}, {len(product(COLORS,)=}")
     #     raise ValueError
 
-    print("="*50)
+    # print("="*50)
+
+    cur_hall = None
+    for row in rows:
+        id, class_name, roll_no, hall, seat_no, subject = row
+        if cur_hall != hall:
+            input("\npress enter to see next hall: ")
+            cur_hall = hall
+            print("\n")
+            print(hall)
+            print("")
+
+        bg, class_color = color_map[class_name]
+        cprint(letter[subject], class_color, bg, end=(
+            "\n" if seat_no % 10 == 0 else ""))
